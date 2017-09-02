a803072d8ad7aa34b8a2dd475e0c80a63159b7ed
Greg Pritchard <gregcraig23@gmail.com>
2017-09-02 16:56:31 -0600

    final commit maybe?

--- code/setup.py
+++ code/setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-  name = 'zalgo',
-  packages = ['zalgo'], # this must be the same as the name above
-  version = '0.3',
+  name = 'zalgotext',
+  packages = ['zalgotext'], # this must be the same as the name above
+  version = '0.4',
   description = 'A small library to process zalgo text',
   author = 'Greg Pritchard',
   author_email = 'gregcraig23@gmail.com',
   url = 'https://github.com/gregoryneal/zalgo', # use the URL to the github repo
-  download_url = 'https://github.com/gregoryneal/zalgo/dist/0.3.tar.gz', # dont change 0.2 fucked up the tags
+  download_url = 'https://github.com/gregoryneal/zalgo/dist/0.4.tar.gz',
   keywords = ['diacritic', 'filter', 'zalgo'], # arbitrary keywords
   classifiers = [],
 )
