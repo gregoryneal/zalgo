59b0c332ec09113b4dc17f63a225b94cde41ab51
Greg Pritchard <gregcraig23@gmail.com>
2017-09-02 16:57:11 -0600

    ha i lied

--- code/setup.py
+++ code/setup.py
@@ -3,7 +3,7 @@ setup(
   name = 'zalgotext',
   packages = ['zalgotext'], # this must be the same as the name above
   version = '0.4',
-  description = 'A small library to process zalgo text',
+  description = 'A small library to create zalgo text',
   author = 'Greg Pritchard',
   author_email = 'gregcraig23@gmail.com',
   url = 'https://github.com/gregoryneal/zalgo', # use the URL to the github repo
