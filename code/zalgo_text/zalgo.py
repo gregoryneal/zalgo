2b5d8528c6fa8297dc8162b73657096af39d1c98
Greg Pritchard <gregcraig23@gmail.com>
2017-09-14 10:30:03 -0600

    new combining logic, now more logical and robust

--- code/zalgo_text/zalgo.py
+++ code/zalgo_text/zalgo.py
@@ -6,6 +6,7 @@ class zalgo():
 		self.numAccentsUp = (1, 3)
 		self.numAccentsDown = (1,3)
 		self.numAccentsMiddle = (1,2)
+		self.maxAccentsPerLetter = 3
 		#downward going diacritics
 		self.dd = ['̖',' ̗',' ̘',' ̙',' ̜',' ̝',' ̞',' ̟',' ̠',' ̤',' ̥',' ̦',' ̩',' ̪',' ̫',' ̬',' ̭',' ̮',' ̯',' ̰',' ̱',' ̲',' ̳',' ̹',' ̺',' ̻',' ̼',' ͅ',' ͇',' ͈',' ͉',' ͍',' ͎',' ͓',' ͔',' ͕',' ͖',' ͙',' ͚',' ',]
 		#upward diacritics
@@ -32,23 +33,30 @@ class zalgo():
 				newLetters.append(a)
 				continue
 
-			num = random.randint(self.numAccentsUp[0],self.numAccentsUp[1])
-			#add the diacritics going up
-			for i in range(num):                            
-				d = self.du[random.randrange(0, len(self.du))].strip()
-				a = self.combine(d, a)
-
-			num = random.randint(self.numAccentsDown[0],self.numAccentsDown[1])
-			#add the diacritics going down
-			for i in range(num):                            
-				d = self.dd[random.randrange(0, len(self.dd))].strip()
-				a = self.combine(d, a)
-
-			num = random.randint(self.numAccentsMiddle[0],self.numAccentsMiddle[1])
-			#add the diacritics in the middle
-			for i in range(num):                            
-				d = self.dm[random.randrange(0, len(self.dm))].strip()
-				a = self.combine(d, a)
+			numAccents = 0
+			numU = random.randint(self.numAccentsUp[0],self.numAccentsUp[1])
+			numD = random.randint(self.numAccentsDown[0],self.numAccentsDown[1])
+			numM = random.randint(self.numAccentsMiddle[0],self.numAccentsMiddle[1])
+			#Try to add accents to the letter, will add an upper, lower, or middle accent randomly until
+			#either numAccents == self.maxAccentsPerLetter or we have added the maximum upper, middle and lower accents. Denoted
+			#by numU, numD, and numM
+			while numAccents < self.maxAccentsPerLetter and numU + numM + numD != 0:
+				randint = random.randint(0,2) # randomly choose what accent type to add
+				if randint == 0:
+					if numU > 0:
+						a = self.combineWithDiacritic(a, self.du)
+						numAccents += 1
+						numU -= 1
+				elif randint == 1:
+					if numD > 0:
+						a = self.combineWithDiacritic(a, self.dd)
+						numD -= 1                    
+						numAccents += 1
+				else:
+					if numM > 0:
+						a = self.combineWithDiacritic(a, self.dm)
+						numM -= 1                    
+						numAccents += 1
 						
 			#a = a.replace(" ","") #remove any spaces, this also gives it the zalgo text look
 			#print('accented a letter: ' + a)
@@ -57,23 +65,18 @@ class zalgo():
 		newWord = ''.join(newLetters)
 		return newWord
 
-	def test(self, *letters):
-		for letter in letters:
-			for d in self.du:
-				print(self.combine(d,letter))
-			for d in self.dd:
-				print(self.combine(d,letter))
-			for d in self.dm:
-				print(self.combine(d,letter))
-
-	def combine(self, diacritic, character):
-		return diacritic.strip()+character.strip()
+	def combineWithDiacritic(self, letter, diacriticList):
+		'''
+		Combines letter and a random character from diacriticList
+		'''
+		return letter.strip() + diacriticList[random.randrange(0, len(diacriticList))].strip()
 
 
 if __name__ == "__main__":
 	z = zalgo()
-	z.numAccentsUp = (0,2)
-	z.numAccentsDown = (0,2)
-	z.numAccentsMiddle = (0,2)
+	z.numAccentsUp = (10,33)
+	z.numAccentsDown = (10,33)
+	z.numAccentsMiddle = (10,33)
+	z.maxAccentsPerLetter = 40
 
-	print(z.zalgofy("A small library to create zalgo styled text."))
+	print(z.zalgofy("Test text"))
